4cc673f17bd7fb8ded8d4d12d172b94355833460
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-08-06 19:40:03 +1000

    Added a service worker with custom URL configuration

---
+++ src/swconfig/sw-build.js
@@ -0,0 +1,27 @@
+const workboxBuild = require("workbox-build");
+// NOTE: This should be run *AFTER* all your assets are built
+const buildSW = () => {
+
+  return workboxBuild
+    .generateSW({
+      swDest: "build/sw.js", // this will be created in the build step
+      globDirectory: "build",
+      globPatterns: [
+        "**/*.{js,css,html,png}"
+      ],
+      navigateFallback: "index.html",
+      cleanupOutdatedCaches: true,
+      runtimeCaching: [
+          
+      ]
+    })
+    .then(({ count, size, warnings }) => {
+      // Optionally, log any warnings and details.
+      warnings.forEach(console.warn);
+      console.log(`${count} files will be precached, totaling ${size} bytes.`);
+    })
+    .catch(err => {
+      console.error(`Unable to generate a new service worker ${err}`);
+    });
+};
+buildSW();
