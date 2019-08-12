006ae179faeb6898a46b1ff0479f34eed6251e7d
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-08-12 14:32:01 +1000

    Added aws -exports.js

---
+++ src/components/auth/SignIn/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SignIn() {
+  return <></>;
+}
+
+export default SignIn;
---
+++ src/components/auth/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from './auth.module.scss';
+
+function Auth() {
+  return (
+    <div className={styled.centreH}>
+      <div className={styled.centreV}>
+        <div className={styled.auth} />
+      </div>
+    </div>
+  );
+}
+
+export default Auth;
