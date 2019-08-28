9d71af052fe8c63374b95f4a7a0cf7e9d86d0312
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-08-28 23:34:34 +1000

    Added logrocket again

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Auth from '@aws-amplify/auth';
+import LogRocket from 'logrocket';
+import setupLogRocketReact from 'logrocket-react';
 import awsconfig from './aws-exports';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 Auth.configure(awsconfig);
+LogRocket.init('ii7gar/comp30022-frontend');
+setupLogRocketReact(LogRocket);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
