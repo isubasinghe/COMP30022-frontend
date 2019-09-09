08ec85a1b809e82a4cd93ad755700ff42aa744c0
Jack Macumber <jmacumber@student.unimelb.edu.au>
2019-09-09 13:22:49 +1000

    Moved Map to Components

--- src/pages/MapView/index.js
+++ src/pages/MapView/index.js
@@ -1,56 +1,8 @@
 import React from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import PropTypes from 'prop-types';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import ArtifactMap from '../../components/map';
 
-// Reload Map Marker
-// TODO: Customize
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-});
-
-class ArtifactMap extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // Default Map Position and Zoom
-    this.state = {
-      lat: 0.0,
-      lon: 0.0,
-      zoom: 2
-    };
-  }
-
-  render() {
-    const artifacts = this.props.artifacts;
-    const mapPosition = [this.state.lat, this.state.lon];
-    return (
-      <Map center={mapPosition} zoom={this.state.zoom} style={{ width: '75%', height: '600px' }}>
-        <TileLayer
-          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        {artifacts.map(arti => (
-          <Marker position={[arti.lat, arti.lon]}>
-            <Popup>
-              <span>
-                <b>{arti.name}</b>
-                <br />
-                {arti.description}
-              </span>
-            </Popup>
-          </Marker>
-        ))}
-      </Map>
-    );
-  }
+function MapView({artifacts}) {
+  return <ArtifactMap artifacts={artifacts} />;
 }
-ArtifactMap.propTypes = {
-  artifacts: PropTypes.node.isRequired
-};
 
-export default ArtifactMap;
+export default MapView;
