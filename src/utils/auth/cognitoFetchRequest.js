0ebdb4442929b41e8346f75f4a6dfb9a857acf3c
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-09-10 13:03:59 +1000

    Added cognito helper

---
+++ src/utils/auth/cognitoFetchRequest.js
@@ -0,0 +1,24 @@
+import Auth from '@aws-amplify/auth';
+
+function authFetchRequest(url, fetchOptions) {
+  return Auth.currentSession()
+    .then(res => {
+      return res.idToken.jwtToken;
+    })
+    .then(token => {
+      const modifiedFetchOptions = fetchOptions;
+      if (modifiedFetchOptions.headers === undefined) {
+        modifiedFetchOptions.headers = {};
+      }
+      modifiedFetchOptions.headers.authorization = token;
+      return fetch(url, fetchOptions);
+    })
+    .then(fetchRes => {
+      return fetchRes.json();
+    })
+    .then(jsonData => {
+      return jsonData;
+    });
+}
+
+export default authFetchRequest;
