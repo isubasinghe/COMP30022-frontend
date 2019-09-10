4c4298c6a3b7cac11c86d617d84d1e7b21f8bcac
Amanda Ni <amandankj98@gmail.com>
2019-09-10 15:04:07 +1000

    background (two parts)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './index.scss';
 
 function Home() {
-  return <></>;
+  return (
+      <div className="home-container">
+        
+      </div>
+  );
 }
 
 export default Home;
