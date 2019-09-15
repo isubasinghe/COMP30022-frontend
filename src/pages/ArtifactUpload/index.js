74df6b6edcae1baf2fd3be7d087c4a15ef16eedb
Amanda Ni <amandankj98@gmail.com>
2019-09-16 01:21:49 +1000

    artifact upload

---
+++ src/pages/ArtifactUpload/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './artifactedit.scss';
+
+function ArtifactEdit() {
+    return (<div>
+        <top-box/>
+        <pre id="logo">
+        AIRLOOM
+        </pre>
+        <pre id="logout" href='/'>
+        LOG OUT
+        </pre>
+        <pre id="name">
+        NAME OF ARTIFACT
+        </pre>
+        <content-box/>
+        <pre id="location">
+        Location
+        </pre>
+        <location-box/>
+        <pre id="date">
+        Date
+        </pre>
+        <date-box/>
+        <pre id="family_members">
+        Family Members
+        </pre>
+        <family-box/>
+        <pre id="descriptions">
+        Descriptions
+        </pre>
+        <descriptions-box/>
+                 <save-button href='/list'>save</save-button >
+                 <cancel-button href='/artifactview'>cancel</cancel-button >
+                 <back-button href='/list'>back</back-button >
+           </div>
+       );
+}
+
+export default ArtifactEdit;
