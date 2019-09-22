b90869d3014ebe7d6393ddec64e65a2e3fe52308
Jack Macumber <jmacumber@student.unimelb.edu.au>
2019-09-22 21:19:09 +1000

    More Buttony

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Button, ButtonToolbar } from 'react-bootstrap';
+import './index.css';
 
 function Nav({ registerId }) {
   return (
-  <ButtonToolbar>
-    <Button href="/">Home</Button>
-    { (registerId !== undefined) 
-      ? (
-          <>
-            <Button href={`/list/${registerId}`}>List</Button>    
-            <Button href={`/map/${registerId}`}>Map</Button>
-            <Button href={`/timeline/${registerId}`}>Timeline</Button>
-          </>
-        )
-      : null
-    }
-  </ButtonToolbar>
+    <ButtonToolbar>
+      {registerId !== undefined ? (
+        <ul>
+          <li><Button href="/">Home</Button></li>
+          <li><Button href={`/list/${registerId}`}>List</Button></li>
+          <li><Button href={`/map/${registerId}`}>Map</Button></li>
+          <li><Button href={`/timeline/${registerId}`}>Timeline</Button></li>
+        </ul>
+      ) : (
+        <Button href="/">Home</Button>
+      )}
+    </ButtonToolbar>
   );
 }
 
