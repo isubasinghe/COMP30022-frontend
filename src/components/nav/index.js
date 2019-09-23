8a2d889426c67dad5fb697c0548ac9e8fd7053d1
Jack Macumber <jmacumber@student.unimelb.edu.au>
2019-09-23 13:20:06 +1000

    css to module.scss

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Button, ButtonToolbar } from 'react-bootstrap';
-import './index.css';
+import styled from './index.module.scss';
 
 function Nav({ registerId }) {
   return (
     <ButtonToolbar>
       {registerId !== undefined ? (
-        <ul>
-          <li><Button href="/">Home</Button></li>
-          <li><Button href={`/list/${registerId}`}>List</Button></li>
-          <li><Button href={`/map/${registerId}`}>Map</Button></li>
-          <li><Button href={`/timeline/${registerId}`}>Timeline</Button></li>
+        <ul className={styled['list-component']}>
+          <li className={styled['list-element']}><Button href="/">Home</Button></li>
+          <li className={styled['list-element']}><Button href={`/list/${registerId}`}>List</Button></li>
+          <li className={styled['list-element']}><Button href={`/map/${registerId}`}>Map</Button></li>
+          <li className={styled['list-element']}><Button href={`/timeline/${registerId}`}>Timeline</Button></li>
         </ul>
       ) : (
         <Button href="/">Home</Button>
