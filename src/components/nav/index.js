31de427c47380b22bed3eaec14fd38b165c8cdc7
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-09-23 13:20:09 +1000

    Fixed to remove null

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -5,23 +5,27 @@ import styled from './index.module.scss';
 
 function Nav({ registerId }) {
   return (
-    <div className={styled['nav']}>
-      <div className={styled['logo']}>airloom</div>
+    <div className={styled.nav}>
+      <div className={styled.logo}>airloom</div>
       <ButtonToolbar className={styled['button-toolbar']}>
-        <Button href="/" className={styled['button']}>Home</Button>
+        <Button href="/" className={styled.button}>
+          Home
+        </Button>
         {registerId !== undefined ? (
           <>
             <LinkContainer to={`/list/${registerId}`}>
-              <Button className={styled['button']}>List</Button>
+              <Button className={styled.button}>List</Button>
             </LinkContainer>
             <LinkContainer to={`/map/${registerId}`}>
-              <Button className={styled['button']}>Map</Button>
+              <Button className={styled.button}>Map</Button>
             </LinkContainer>
             <LinkContainer to={`/timeline/${registerId}`}>
-              <Button className={styled['button']}>Timeline</Button>
+              <Button className={styled.button}>Timeline</Button>
             </LinkContainer>
           </>
-        ) : null}
+        ) : (
+          <></>
+        )}
       </ButtonToolbar>
     </div>
   );
