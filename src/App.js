b8ca9826c671ab4a12a91b287bfb93aca7d5a298
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-09-27 14:40:44 +1000

    Completely removed logrocket

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { withAuthenticator } from 'aws-amplify-react';
 import Auth from '@aws-amplify/auth';
-import LogRocket from 'logrocket';
 import AirNavBar from './components/navbar';
 import Home from './pages/Home';
 import MapView from './pages/MapView';
@@ -18,44 +17,31 @@ function App() {
   const [registers, setRegisters] = useState([]);
   const refetchRegisters = () => {
     getRegisters()
-      .then(registers => {
+      .then(registersFetched => {
         if (registers !== null) {
-          setRegisters(registers);
+          setRegisters(registersFetched);
         }
       })
-      .catch(err => {
-        LogRocket.error(err);
-      });
+      .catch(err => {});
   };
   useEffect(() => {
     Auth.currentAuthenticatedUser({
       bypassCache: false
     })
-      .then(user => {
-        LogRocket.identify({
-          name: user.attributes.name,
-          email: user.attributes.email,
-          userID: user.username
-        });
+      .then(() => {
         return getDefaultRegister();
       })
       .then(reg => {
         localStorage.setItem('reg', JSON.stringify(reg));
       })
-      .catch(() => {
-        LogRocket.identify({
-          name: 'Anonymous'
-        });
-      });
+      .catch(() => {});
     getRegisters()
       .then(registersFetched => {
         if (registersFetched !== null) {
           setRegisters(registersFetched);
         }
       })
-      .catch(error => {
-        LogRocket.error(error);
-      });
+      .catch(error => {});
   }, []);
 
   return (
