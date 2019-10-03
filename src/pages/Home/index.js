3c510f2392000ecfb65473d980a40e26b8f8a1f4
codefactor-io <support@codefactor.io>
2019-10-03 14:11:13 +0000

    [CodeFactor] Apply fixes to commit 859e619

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,12 +3,10 @@ import styled from './index.module.scss';
 
 function Home() {
   return (
-    <> 
-      <div className={styled["banner-top"]}/>
-      <p className={styled["title"]}>
-        WELCOME TO AIRLOOM
-      </p>
-      <div className={styled["banner-bottom"]}/>
+    <>
+      <div className={styled['banner-top']} />
+      <p className={styled.title}>WELCOME TO AIRLOOM</p>
+      <div className={styled['banner-bottom']} />
     </>
   );
 }
