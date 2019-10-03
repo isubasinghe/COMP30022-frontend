859e61950e946eddaa5a5fef2be32dc094328376
James Edward Barnes <barnesj2@student.unimelb.edu.au>
2019-10-04 00:07:27 +1000

    added home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from './index.module.scss';
 
 function Home() {
-  return <></>;
+  return (
+    <> 
+      <div className={styled["banner-top"]}/>
+      <p className={styled["title"]}>
+        WELCOME TO AIRLOOM
+      </p>
+      <div className={styled["banner-bottom"]}/>
+    </>
+  );
 }
 
 export default Home;
