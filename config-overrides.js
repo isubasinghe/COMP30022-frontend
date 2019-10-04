b22acbc7470f5e75e1d2861f6e6f1f4a37573aaa
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-10-04 12:37:01 +1000

    Added minification for image assets

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const path = require('path');
 
 module.exports = (config, env) => {
@@ -8,6 +9,12 @@ module.exports = (config, env) => {
       new PrerenderSPAPlugin({
         routes: ['/'],
         staticDir: path.join(__dirname, 'build')
+      }),
+      new ImageminPlugin({
+        disable: process.env.NODE_ENV !== 'production', // Disable during development
+        pngquant: {
+          quality: '95-100'
+        }
       })
     ]);
   }
