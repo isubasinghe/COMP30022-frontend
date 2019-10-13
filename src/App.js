6180b44b30307b59a9c09c0b17d541d58ecf25ca
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2019-10-13 13:40:09 +1100

    Added lazy css import, loading screen

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import Loadable from 'react-loadable';
 import Home from './pages/Home';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import('bootstrap/dist/css/bootstrap.min.css')
+  .then(() => {})
+  .catch(() => {});
 
 const LoadableApp = Loadable({
   loader: () => import('./AppEntry'),
