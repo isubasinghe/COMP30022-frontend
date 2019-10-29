9ca890d59700354da246cccf092f369a36e517cf
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2019-10-29 18:34:26 +1100

    Added basic unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,12 @@
-it('renders without crashing', () => {});
+import React from 'react';
+import NavBar from './components/navbar';
+import Map from './components/map';
+import { shallow } from 'enzyme';
+
+it('navbar renders without crashing', () => {
+  const mount = shallow(<NavBar refetch={() => {}} registers={[]} history={{ push: () => {} }} />);
+});
+
+it('map renders without crashing', () => {
+  const mount = shallow(<Map className="" />);
+});
