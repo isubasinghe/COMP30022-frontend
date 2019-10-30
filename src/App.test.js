1acd084094d6e7d273496e44b3aea1317be32ec2
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2019-10-30 21:50:55 +1100

    Removed App.test.js

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Map from './components/map';
-
-it('map renders without crashing', () => {
-  shallow(<Map className="" />);
-});
