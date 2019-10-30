2324c1009425acfa7fbf9c63226d3256102ddee5
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2019-10-30 22:05:53 +1100

    Added some more testing

--- src/components/navbar/index.test.js
+++ src/components/navbar/index.test.js
@@ -5,3 +5,19 @@ import NavBar from './index';
 it('navbar renders without crashing', () => {
   shallow(<NavBar refetch={() => {}} registers={[]} history={{ push: () => {} }} />);
 });
+
+it('navbar renders registers with empty names', () => {
+  shallow(
+    <NavBar
+      refetch={() => {}}
+      registers={[
+        {
+          register_id: 0,
+          name: '',
+          is_admin: false
+        }
+      ]}
+      history={{ push: () => {} }}
+    />
+  );
+});
